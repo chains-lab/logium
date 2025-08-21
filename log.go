0ca105bba41bbef4d48ec9c3083fca23dd88dbda
webzavet <yehorushakov@gmail.com>
2025-08-22 02:33:44 +0300

    first commit

---
+++ log.go
@@ -0,0 +1,59 @@
+package logger
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/chains-lab/svc-errors/ape"
+	"github.com/sirupsen/logrus"
+)
+
+func NewLogger(level, format string) *logrus.Logger {
+	log := logrus.New()
+
+	lvl, err := logrus.ParseLevel(strings.ToLower(level))
+	if err != nil {
+		log.Warnf("invalid log level '%s', defaulting to 'info'", level)
+		lvl = logrus.InfoLevel
+	}
+	log.SetLevel(lvl)
+
+	switch strings.ToLower(format) {
+	case "json":
+		log.SetFormatter(&logrus.JSONFormatter{})
+	case "text":
+		fallthrough
+	default:
+		log.SetFormatter(&logrus.TextFormatter{
+			FullTimestamp: true,
+		})
+	}
+
+	return log
+}
+
+type Logger interface {
+	WithError(err error) *logrus.Entry
+
+	logrus.FieldLogger
+}
+
+type logger struct {
+	*logrus.Entry
+}
+
+func (l *logger) WithError(err error) *logrus.Entry {
+	var ae *ape.Error
+	if errors.As(err, &ae) {
+		return l.Entry.WithError(ae.Unwrap())
+	}
+	return l.Entry.WithError(err)
+}
+
+func NewWithBase(base *logrus.Logger) Logger {
+	log := logger{
+		Entry: logrus.NewEntry(base),
+	}
+
+	return &log
+}
